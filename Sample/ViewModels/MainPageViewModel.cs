075c4d285ca49480569eaf053d4b392a8aae9312
kamu <kamu@kamunoMacBook-Pro.local>
2017-05-08 00:24:03 +0900

    Default ViewCell

---
+++ Sample/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,57 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections.ObjectModel;
+
+namespace Sample.ViewModels
+{
+	public class MainPageViewModel : BindableBase, INavigationAware
+	{
+		public ObservableCollection<PhotoItem> Items{get;set;}
+
+		private string _title;
+		public string Title {
+			get { return _title; }
+			set { SetProperty(ref _title, value); }
+		}
+
+		public MainPageViewModel()
+		{
+			Items = new ObservableCollection<PhotoItem>();
+			for(var i=0;i<20;i++){
+				Items.Add(new PhotoItem{
+					PhotoUrl=$"http://kamusoft.jp/openimage/nativecell/{i+1}.jpg",
+					Title = "たいとる",
+					Date = "2017-01-11 11:11:11"
+				});
+			}
+		}
+
+		public void OnNavigatedFrom(NavigationParameters parameters)
+		{
+
+		}
+
+		public void OnNavigatedTo(NavigationParameters parameters)
+		{
+			if (parameters.ContainsKey("title"))
+				Title = (string)parameters["title"] + " and Prism";
+		}
+
+		public void OnNavigatingTo(NavigationParameters parameters)
+		{
+		}
+
+		public class PhotoItem
+		{
+			public string PhotoUrl { get; set; }
+			public string Title { get; set; }
+			public string Date { get; set; }
+
+		}
+	}
+}
+
